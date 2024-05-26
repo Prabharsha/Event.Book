01b90041c243b3b4b891b8f6a7cd9954d00b0de4
Prabharsha <89606147+Prabharsha@users.noreply.github.com>
2024-05-27 01:21:05 +0530

    user creation bug fixed

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,7 +4,7 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
 
     clerkId: { type: String, required: true, unique: true },
-    emai: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
